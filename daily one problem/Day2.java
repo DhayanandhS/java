462d21f09314e7df8f985f5bf8a5c0703d24b09a
DhayanandhS <dhaya.pit180@gmail.com>
2024-03-29 19:03:42 +0530

    updated

---
+++ daily one problem/Day2.java
@@ -0,0 +1,11 @@
+public class Day2 {
+    public static void main(String[] args) {
+        int rows = 5;
+        for (int i = rows; i >= 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
