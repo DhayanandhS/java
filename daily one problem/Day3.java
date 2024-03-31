0f2469ebdfdc5a5612372b84df9b976b24a5d92f
DhayanandhS <dhaya.pit180@gmail.com>
2024-03-31 15:13:41 +0530

    added s.close();

--- daily one problem/Day3.java
+++ daily one problem/Day3.java
@@ -2,10 +2,11 @@ import java.util.*;
 public class Day3 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        int n = s.nextInt();// Number of terms
+        int n = s.nextInt();
+        s.close();// Number of terms
         int a = 0, b = 1;
         System.out.println("Fibonacci Series up to " + n + " terms:");
-        for (int i = 1; i <= n; ++i) {
+        for (int i = 1; i <= n; ++i) {//we can also give i++ in the for loop//
             System.out.print(a + " ");
             int sum = a + b;
             a = b;
