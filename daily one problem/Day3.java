b0aeb0b100dccb5814745ad01c533c112590a181
DhayanandhS <dhaya.pit180@gmail.com>
2024-03-31 15:03:28 +0530

    added day 4 programs

---
+++ daily one problem/Day3.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Day3 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();// Number of terms
+        int a = 0, b = 1;
+        System.out.println("Fibonacci Series up to " + n + " terms:");
+        for (int i = 1; i <= n; ++i) {
+            System.out.print(a + " ");
+            int sum = a + b;
+            a = b;
+            b = sum;
+        }
+    }
+}
