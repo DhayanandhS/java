d9ed019817ca7cf7994bbdf8f18ceb7d314da85e
DhayanandhS <dhaya.pit180@gmail.com>
2024-04-06 22:44:09 +0530

    added day6 problem

---
+++ daily one problem/Day6.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Day6{
+    public static void main(String args[]){
+        System.out.println("Prime number finding problem");
+        Scanner s = new Scanner(System.in);
+        int x = s.nextInt();
+        if (x%2!=0){
+            if(x)
+            if(x%x==1){
+                System.out.println("x is prime number");
+            }
+        }
+    }
+}
