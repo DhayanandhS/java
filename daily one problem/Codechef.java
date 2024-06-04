4b63303f83376b1947d7e0840a49ade8722da4c0
DhayanandhS <dhaya.pit180@gmail.com>
2024-06-04 11:22:51 +0530

    codechef

---
+++ daily one problem/Codechef.java
@@ -0,0 +1,10 @@
+public class Codechef {
+    public static void main(String[] args) {
+      int a = 4;
+      int b = 1;
+      while (a != b) {
+        System.out.println(b);
+        b = b + 1;
+      }
+    }
+  }
