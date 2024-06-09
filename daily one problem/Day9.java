fcf72295622653d182064ed869ccc7b964841eb4
DhayanandhS <dhaya.pit180@gmail.com>
2024-06-09 08:50:14 +0530

    Day13

--- daily one problem/Day9.java
+++ daily one problem/Day9.java
@@ -3,7 +3,7 @@ class Day9
 	public static void main (String[] args)
 	{
 	   // Update the blank in the code given below.
-		int i = 1;
+		int i = 0;
 		do{
 		    System.out.println(i);
 		    i++;
