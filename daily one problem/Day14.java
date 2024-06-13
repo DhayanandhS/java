d838aca34cbaa5943a1da10b503369946a462dcc
DhayanandhS <dhaya.pit180@gmail.com>
2024-06-13 11:08:08 +0530

    added Day14 problem

---
+++ daily one problem/Day14.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public static void nNumberTriangle(int n) {
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n-i+1; j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
