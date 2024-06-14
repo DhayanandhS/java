4a9640959b69fcfbe24ea2199cfa5e3a2de95850
DhayanandhS <dhaya.pit180@gmail.com>
2024-06-15 00:12:23 +0530

    added Day18.java

---
+++ daily one problem/Day18.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+	    int i = 5; // Initialization
+		while (i < 21) { // Condition
+            System.out.println(i);
+            i = i+1; // Update statement
+        }
+
+	}
+}
