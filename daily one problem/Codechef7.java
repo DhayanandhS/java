63a87016cf531809cae11ca6a654d7e75bd4f5ba
DhayanandhS <dhaya.pit180@gmail.com>
2024-06-15 00:13:26 +0530

    added Codechef7.java

---
+++ daily one problem/Codechef7.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+	   // Update the code below this line.
+	    int i = 11;
+	    while(i < 20){
+	        System.out.println(i);
+	        i = i+2;
+	        
+	    }
+	}
+}
