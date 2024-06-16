e3681889dcd9d8f3fdbab1058e56e42d9da07d65
DhayanandhS <dhaya.pit180@gmail.com>
2024-06-16 22:35:08 +0530

    added Codechef11.java

---
+++ daily one problem/Codechef11.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    int height1 = 15;
+	    int height2 = 13;
+	    // Update the blanks below this line
+	    System.out.println(height1 == height2);
+	}
+}
+
