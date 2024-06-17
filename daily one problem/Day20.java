a44d8053ba19acc7c9cd5fd5f8d7280dabd47d8d
DhayanandhS <dhaya.pit180@gmail.com>
2024-06-17 20:27:17 +0530

    added Day20 file

---
+++ daily one problem/Day20.java
@@ -0,0 +1,9 @@
+class Day20
+{
+	public static void main (String[] args)
+	{
+		String[] week = {"Monday", "Tuesday", "Wednesday", "Thursday"};
+		System.out.println(week[2]);
+		System.out.println(week[3]);
+	}
+}
