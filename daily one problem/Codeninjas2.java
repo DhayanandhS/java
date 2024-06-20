c887b17aaf03ca6f1ed2c4296516a02babd3b91f
DhayanandhS <dhaya.pit180@gmail.com>
2024-06-20 13:13:21 +0530

    added Codeninjas2.java

---
+++ daily one problem/Codeninjas2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution {
+	
+	public static void main(String args[]) {
+		
+		Scanner sc = new Scanner(System.in);
+		String name = sc.nextLine();
+		int age = sc.nextInt();
+		System.out.println("The name of the person is "+name+" and the age is "+age+".");
+		
+	}
+}
