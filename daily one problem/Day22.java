1e29e91ed0eb93ceae3be1afc1b7542214d03c6a
DhayanandhS <dhaya.pit180@gmail.com>
2024-06-20 13:08:07 +0530

    added Day22.java

---
+++ daily one problem/Day22.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Solution {
+	
+	public static void main(String args[]) {
+		
+		Scanner sc = new Scanner(System.in);
+		int L = sc.nextInt();
+		int B = sc.nextInt();
+		System.out.println(L*B);
+	}
+}
