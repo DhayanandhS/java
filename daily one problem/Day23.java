5cdcb6821184d83ac2344a0c7a8b59d10e67d2f7
DhayanandhS <dhaya.pit180@gmail.com>
2024-06-21 22:58:03 +0530

    added Day23.java

---
+++ daily one problem/Day23.java
@@ -0,0 +1,7 @@
+public class Day23 {
+  
+    public static void main(String[] args) {
+           // Display the current date and time using System.currentTimeMillis()
+           System.out.format("\nCurrent Date time: %tc%n\n", System.currentTimeMillis());
+      }
+  }
