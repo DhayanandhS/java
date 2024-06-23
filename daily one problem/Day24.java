ebcf672c0cd79d877c006ac3e784c4350d9cc63b
DhayanandhS <dhaya.pit180@gmail.com>
2024-06-23 20:21:18 +0530

    Added Day24.java

---
+++ daily one problem/Day24.java
@@ -0,0 +1,10 @@
+public class Exercise8 {
+ 
+    public static void main(String[] args) {
+        // Display the characters to form the text "Java" in a specific pattern
+        System.out.println("   J    a   v     v  a ");
+        System.out.println("   J   a a   v   v  a a");
+        System.out.println("J  J  aaaaa   V V  aaaaa");
+        System.out.println(" JJ  a     a   V  a     a");
+    }
+} 
