805bc0fa38190cbe86f8818f600832eb5947fd3b
DhayanandhS <dhaya.pit180@gmail.com>
2024-06-28 14:22:23 +0530

    added Codechef17.java

---
+++ daily one problem/Codechef17.java
@@ -0,0 +1,17 @@
+class Codechef
+{
+	public static void main (String[] args)
+	{
+        int a = 2;
+        int b = 3;
+        int c = 4;
+        System.out.println(a*a);
+        System.out.println(a*a*a);
+        System.out.println(b*b);
+        System.out.println(b*b*b);
+        System.out.println(c*c);
+        System.out.println(c*c*c);
+
+	}
+}
+
