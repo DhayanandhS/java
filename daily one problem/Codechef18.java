9e72d1fdc5ae91a9ff1acab47ac3426a787d5715
DhayanandhS <dhaya.pit180@gmail.com>
2024-06-28 14:23:38 +0530

    added Codechef18.java

---
+++ daily one problem/Codechef18.java
@@ -0,0 +1,20 @@
+class Codechef
+{
+    
+    public static void squareAndCube(int x) 
+    {
+        System.out.println(x * x);
+        System.out.println(x * x * x);
+    }
+    
+	public static void main (String[] args)
+	{
+        int a = 2;
+        int b = 3;
+        int c = 4;
+        
+        squareAndCube(a);
+        squareAndCube(b);
+        squareAndCube(c);
+	}
+}
