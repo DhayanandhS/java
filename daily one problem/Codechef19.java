48748c93f9994ad19091361796a672c2405a3fe1
DhayanandhS <dhaya.pit180@gmail.com>
2024-06-29 11:52:29 +0530

    added Codechef19.java file

---
+++ daily one problem/Codechef19.java
@@ -0,0 +1,13 @@
+class Codechef
+{
+	public static void main (String[] args)
+	{
+        greet("Alice");
+    }
+
+    // User-defined method to greet a person by name
+    public static void greet(String name) 
+    {
+        System.out.println("Hello, " + name + "!");
+	}
+}
