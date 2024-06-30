b1cb1831e2127210e97230eacc2cebbeb16ef38f
DhayanandhS <dhaya.pit180@gmail.com>
2024-06-30 20:27:51 +0530

    added Day29.java

---
+++ daily one problem/Day29.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+        Scanner read = new Scanner(System.in);
+        
+        String A = read.next();
+        String B = read.next();
+        String C = read.next();
+        String D = read.next();
+        String E = read.next();
+        // You can use "\n" to add a new line instead of adding multiple print statements
+        System.out.print(A + "\n" + B + "\n" + C + "\n" + D + "\n" + E );
+	}
+}
