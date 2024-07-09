45449e9d383815d7c6a24b56f52e64190a27c678
DhayanandhS <dhaya.pit180@gmail.com>
2024-07-09 22:45:21 +0530

    added Day34.java

---
+++ daily one problem/Day34.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public static void main(String args[]) {
+        do {
+            System.out.println("Coding Ninjas");
+        }
+
+        while(true);
+
+        System.out.println("Ninjas"); 
+    }
+}
