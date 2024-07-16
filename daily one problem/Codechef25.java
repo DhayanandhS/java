c40718899163acbcb326a4037df1c3b198dd378e
DhayanandhS <dhaya.pit180@gmail.com>
2024-07-16 22:14:23 +0530

    Janmansh and Assignments

---
+++ daily one problem/Codechef25.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		
+		for(int i = 0; i < t; i++){
+		    
+		    int x = sc.nextInt();
+		    
+		    if(x <= 7){
+		        System.out.println("Yes");
+		    }
+		    else{
+		        System.out.println("No");
+		    }
+		}
+	}
+}
