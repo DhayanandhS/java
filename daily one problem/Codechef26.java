41bbe423d158a659bdb7478c7e21a22a9addd5ef
DhayanandhS <dhaya.pit180@gmail.com>
2024-07-19 23:13:24 +0530

    Chef in his office

---
+++ daily one problem/Codechef26.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	   Scanner in =new Scanner(System.in);
+	   int t=in.nextInt();
+	   while(t-->0){
+	       int x=in.nextInt();
+	       int y=in.nextInt();
+	       System.out.println(4*x+y*1);
+	   }
+	}
+}
