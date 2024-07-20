ebe9b517772f7d339499324936f0bf65784db889
DhayanandhS <dhaya.pit180@gmail.com>
2024-07-20 18:18:54 +0530

    Total Salary

---
+++ daily one problem/Day38.java
@@ -0,0 +1,21 @@
+import java.util.* ;
+import java.io.*; 
+class Solution {
+	
+	public static void main(String args[]) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+
+		if(a > b && a > c){
+				System.out.println(a);
+		}
+		else if(b > c){
+			System.out.println(b);
+		}
+		else System.out.println(c);
+	}
+}
