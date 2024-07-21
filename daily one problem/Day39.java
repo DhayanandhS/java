2a66798ba38dfffc730bccac560db8146f6977f9
DhayanandhS <dhaya.pit180@gmail.com>
2024-07-21 17:38:06 +0530

    added all the intern certificates to the resume page

---
+++ daily one problem/Day39.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		int t = sc.nextInt();
+		
+		for(int i = 0; i < t; i++){
+		    int x = sc.nextInt();
+		    int y = sc.nextInt();
+		    
+		    int z = x * y;
+		    
+		    System.out.println( z / 100 );
+		}
+
+	}
+}
