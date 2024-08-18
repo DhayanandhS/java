260e6f222a069834dd182db411e8873d2085bedd
DhayanandhS <dhaya.pit180@gmail.com>
2024-08-18 18:45:51 +0530

    Hashing 1

---
+++ daily one problem/Day42.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Day42 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n;
+        n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        //precompute:
+        int[] hash = new int[13];
+        for (int i = 0; i < n; i++) {
+            hash[arr[i]] += 1;
+        }
+
+        int q;
+        q = sc.nextInt();
+        while (q-- != 0) {
+            int number;
+            number = sc.nextInt();
+            // fetching:
+            System.out.println(hash[number]);
+        }
+    }
+}
